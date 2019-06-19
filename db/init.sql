caf35412e04d5b48d8cbea8a4ce13665c9ab7164
Tim Nash <tnash3@apple.com>
2019-06-18 18:52:36 -0700

    clean up some ui

--- db/init.sql
+++ db/init.sql
@@ -1,55 +1,39 @@
--- CREATE DATABASE knights;
--- use knights;
+CREATE DATABASE knights;
+use knights;
 
--- CREATE TABLE favorite_colors (
---   name VARCHAR(20),
---   color VARCHAR(10)
--- );
 
--- INSERT INTO favorite_colors
---   (name, color)
--- VALUES
---   ('Lancelot', 'blue'),
---   ('Galahad', 'yellow');
+CREATE TABLE favorite_colors (
+  name VARCHAR(20),
+  color VARCHAR(10)
+);
 
--- MySQL dump 10.13  Distrib 8.0.15, for osx10.14 (x86_64)
---
--- Host: localhost    Database: knights
--- ------------------------------------------------------
--- Server version	5.7.26
+INSERT INTO favorite_colors
+  (name, color)
+VALUES
+  ('Lancelot', 'blue'),
+  ('Galahad', 'yellow');
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8mb4 ;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
 
 --
 -- Table structure for table `alembic_version`
 --
 
-DROP TABLE IF EXISTS `alembic_version`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `alembic_version` (
-  `version_num` varchar(32) NOT NULL,
-  PRIMARY KEY (`version_num`)
+DROP TABLE IF EXISTS alembic_version;
+
+CREATE TABLE alembic_version (
+  version_num varchar(32) NOT NULL,
+  PRIMARY KEY (version_num)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Table structure for table `post`
 --
 
-DROP TABLE IF EXISTS `post`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `post` (
+DROP TABLE IF EXISTS post;
+
+CREATE TABLE post (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `body` text,
   `timestamp` datetime DEFAULT NULL,
@@ -59,17 +43,16 @@ CREATE TABLE `post` (
   KEY `ix_post_timestamp` (`timestamp`),
   CONSTRAINT `post_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS 'user';
+
+CREATE TABLE user (
+  'id' int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(64) DEFAULT NULL,
   `email` varchar(120) DEFAULT NULL,
   `password_hash` varchar(128) DEFAULT NULL,
@@ -77,15 +60,3 @@ CREATE TABLE `user` (
   UNIQUE KEY `ix_user_email` (`email`),
   UNIQUE KEY `ix_user_username` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-06-18 16:57:27
