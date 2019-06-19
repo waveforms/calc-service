5e76d9acb2afeeea666ee0102dbf6bc4f31d334a
Tim Nash <tnash3@apple.com>
2019-06-18 21:16:29 -0700

    update database creation

--- db/init.sql
+++ db/init.sql
@@ -1,62 +1,18 @@
 CREATE DATABASE knights;
 use knights;
 
-
-CREATE TABLE favorite_colors (
-  name VARCHAR(20),
-  color VARCHAR(10)
-);
-
-INSERT INTO favorite_colors
-  (name, color)
-VALUES
-  ('Lancelot', 'blue'),
-  ('Galahad', 'yellow');
-
-
-
---
--- Table structure for table `alembic_version`
---
-
-DROP TABLE IF EXISTS alembic_version;
-
-CREATE TABLE alembic_version (
-  version_num varchar(32) NOT NULL,
-  PRIMARY KEY (version_num)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
---
--- Table structure for table `post`
---
-
-DROP TABLE IF EXISTS post;
+CREATE TABLE IF NOT EXISTS user (
+  id INT AUTO_INCREMENT,
+  username VARCHAR(64),
+  email VARCHAR(120),
+  password_hash VARCHAR(128),
+  PRIMARY KEY (id)
+) ENGINE=INNODB;
 
 CREATE TABLE post (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `body` text,
-  `timestamp` datetime DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  KEY `ix_post_timestamp` (`timestamp`),
-  CONSTRAINT `post_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
-
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS 'user';
-
-CREATE TABLE user (
-  'id' int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(64) DEFAULT NULL,
-  `email` varchar(120) DEFAULT NULL,
-  `password_hash` varchar(128) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `ix_user_email` (`email`),
-  UNIQUE KEY `ix_user_username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  id int(11) AUTO_INCREMENT,
+  body text,
+  timestamp datetime,
+  user_id int(11),
+  PRIMARY KEY (id)
+) ENGINE=INNODB;
